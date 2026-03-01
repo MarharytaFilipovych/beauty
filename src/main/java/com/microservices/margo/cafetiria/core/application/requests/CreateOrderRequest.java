226fa60ba5db292d3ee53330a7249e1df5353380
Marharyta Filipovych <marharyta.filipovych@cantire.com>
2026-03-01 19:10:54 +0200

    Added request dtos

---
+++ src/main/java/com/microservices/margo/cafetiria/core/application/requests/CreateOrderRequest.java
@@ -0,0 +1,14 @@
+package com.microservices.margo.cafetiria.core.application.requests;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.math.BigDecimal;
+
+public record CreateOrderRequest(
+        @NotBlank String customerName,
+        @NotBlank String itemName,
+        @Min(value = 1) int quantity,
+        @NotNull @Positive BigDecimal price
+) {}
