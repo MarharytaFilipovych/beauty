1613cfe2189d5c97bcfde2db242aff29353e6f6e
Marharyta Filipovych <marharyta.filipovych@cantire.com>
2026-03-09 18:12:53 +0200

    order created eventt setup (publisher and modied createorderusecase

--- order-service/src/main/java/com/microservices/margo/order_service/core/application/usecase/CreateOrderUseCase.java
+++ order-service/src/main/java/com/microservices/margo/order_service/core/application/usecase/CreateOrderUseCase.java
@@ -5,6 +5,7 @@ import com.microservices.margo.order_service.core.application.mapper.OrderMapper
 import com.microservices.margo.order_service.core.domain.Order;
 import com.microservices.margo.order_service.core.infrastructure.client.UserValidationClient;
 import com.microservices.margo.order_service.core.infrastructure.entity.OrderEntity;
+import com.microservices.margo.order_service.core.infrastructure.messaging.OrderEventPublisher;
 import com.microservices.margo.order_service.core.infrastructure.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,7 @@ public class CreateOrderUseCase {
     private final OrderMapper orderMapper;
     private final OrderRepository orderRepository;
     private final UserValidationClient userValidationClient;
+    private final OrderEventPublisher eventPublisher;
 
     public Order execute(CreateOrderRequest command) {
         log.info("Validating user {} before placing order", command.ownerUserId());
@@ -25,6 +27,8 @@ public class CreateOrderUseCase {
         log.info("Placing order for customer='{}', item='{}'",
                 command.ownerUserId(), command.itemName());
         OrderEntity entity = orderRepository.save(orderMapper.toEntity(command));
-        return orderMapper.toDomain(entity);
+        Order order = orderMapper.toDomain(entity);
+        eventPublisher.publishOrderCreated(order);
+        return order;
     }
 }
