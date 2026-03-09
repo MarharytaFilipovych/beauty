27e559b5190e40e4fb39e62f38dc211cfeef228c
Marharyta Filipovych <marharyta.filipovych@cantire.com>
2026-03-09 18:07:42 +0200

    rabbitmq setup

--- order-service/src/main/java/com/microservices/margo/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/microservices/margo/order_service/OrderServiceApplication.java
@@ -1,9 +1,12 @@
 package com.microservices.margo.order_service;
 
+import com.microservices.margo.order_service.core.infrastructure.config.RabbitMQProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RabbitMQProperties.class)
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
