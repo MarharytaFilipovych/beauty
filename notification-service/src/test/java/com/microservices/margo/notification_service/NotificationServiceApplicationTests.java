c2923ce2b8d035ec4a35f166c62fa5c214295e20
Marharyta Filipovych <marharyta.filipovych@cantire.com>
2026-03-09 21:29:31 +0200

    fixes

--- notification-service/src/test/java/com/microservices/margo/notification_service/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/com/microservices/margo/notification_service/NotificationServiceApplicationTests.java
@@ -2,13 +2,13 @@ package com.microservices.margo.notification_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+@SpringBootTest
+@TestPropertySource(properties = {
         "spring.flyway.enabled=false",
-        "spring.jpa.hibernate.ddl-auto=create-drop"
+        "spring.jpa.hibernate.ddl-auto=none",
+        "spring.datasource.url=jdbc:h2:mem:testdb"
 })
 class NotificationServiceApplicationTests {
 
