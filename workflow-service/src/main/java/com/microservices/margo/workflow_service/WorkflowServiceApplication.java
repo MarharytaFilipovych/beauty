52a7b117c18216cf5a2c126c9c7e6dcfa2811b31
Marharyta Filipovych <marharyta.filipovych@cantire.com>
2026-03-13 20:31:22 +0200

    Configured services, client calls, mapper, request model and other models, workflow controller

--- workflow-service/src/main/java/com/microservices/margo/workflow_service/WorkflowServiceApplication.java
+++ workflow-service/src/main/java/com/microservices/margo/workflow_service/WorkflowServiceApplication.java
@@ -1,9 +1,12 @@
 package com.microservices.margo.workflow_service;
 
+import com.microservices.margo.workflow_service.core.infrastructure.config.OrderServiceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OrderServiceProperties.class)
 public class WorkflowServiceApplication {
 
 	public static void main(String[] args) {
